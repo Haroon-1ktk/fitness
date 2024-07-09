6e355d0ac74f3c840b130ef1b37c28e5cae55773
Haroon-1ktk <121959975+Haroon-1ktk@users.noreply.github.com>
2024-07-09 12:55:11 -0700

    initial commit

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
   return (
     <>
     <section className='flex flex-col md:flex-row'>
-    <div className='mt-24 ml-12 w-[550px]'>
+    <div className='mt-24 ml-12 md:w-[550px]'>
       <div >
         <h1 className='text-red-600 text-lg pb-1 font-semibold'>Fitness Club</h1>
         <h2 className='font-bold text-xl mb-6 mt-7'>Sweat,Smile and Repeat</h2>
@@ -16,7 +16,7 @@ const Banner = () => {
          <h1 className='md:block text-red-200 hidden text-[200px]'>Excercises</h1>
       </div>
     </div>
-  <div className='mt-4 w-[750px]'>
+  <div className='mt-4 w-[750px] md:block hidden'>
   <img src="https://images.unsplash.com/photo-1526506118085-60ce8714f8c5?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
    alt="Fitness Freek" 
    className='h-[500px] w-full z-20 rounded bg-cover'/>
--- src/Components/Searchbar.jsx
+++ src/Components/Searchbar.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Searchbar = () => {
+  const [search,setSearch]=useState('');
+  const handleSearch=()=>{
+
+  }
   return (
-    <div>Searchbar</div>
+    <>
+    <div>
+      <h1 className='text-center mb-6 text-6xl'>Awesome Exercises You<br/>Should Know</h1>
+      <form className='flex justify-center items-center' onSubmit={handleSearch}>
+     <input 
+     type="text"  
+     value={search}
+     onChange={(e)=>setSearch(e.target.value.toLowerCase())}
+     placeholder='Search Exercises'
+     className='md:w-[800px] w-64 pl-2 bg-white rounded placeholder:text-gray-400 outline-none border-2 border-gray-400 h-10'/>
+     <button className='hover:border-2 border-gray-400 bg-red-600 text-white rounded px-6 py-2 hover:bg-white hover:text-red-600'>Search</button>
+        </form>
+    </div>
+    </>
   )
 }
 
